bc84d22590e625a3314507edb1bbbbb08db36616
luisfsborsari <luisfsborsari@gmail.com>
2017-10-03 20:54:45 -0300

    teste

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
